debff8ac9a46e4b6a5f14a8f838808777fb9fd02
bhwa233 <404174262@qq.com>
2023-10-22 10:03:22 +0800

    commit 2023年10月22日 10:03:22

--- promise/promise.js
+++ promise/promise.js
@@ -3,86 +3,86 @@ const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 
 // base
-class myPromise {
-  constructor(fn) {
-    this.status = 'pending'
-    this.value = null
-    this.resQueue = []
-    this.rejQueue = []
-    let resolve = (value) => {
-      setTimeout(() => {
-        if (this.status !== 'pending') return
-        this.status = 'fulfilled'
-        this.value = value
-        for (let callback of this.resQueue) {
-          callback(value)
-        }
-      })
-    }
-    let reject = (reason) => {
-      setTimeout(() => {
-        if (this.status !== PENDING) return
-        this.status = REJECTED
-        this.data = null
-        for (let callback of this.rejQueue) {
-          callback(reason)
-        }
-      })
-    }
-    fn(resolve, reject)
-  }
-  then(resFn, rejFn) {
-    return new Promise((res, rej) => {
-      let fulfilledFn = (v) => {
-        try {
-          res(resFn(v))
-        } catch (error) {
-          rej(error)
-        }
-      }
-      let rejectedFn = (v) => {
-        try {
-          res(resFn(rejFn))
-        } catch (error) {
-          rej(error)
-        }
-      }
-      switch (this.status) {
-        case PENDING:
-          this.resQueue.push(fulfilledFn);
-          this.rejQueue.push(rejectedFn)
-          break
-        case FULFILLED:
-          fulfilledfn(this.data)
-          break
-        case REJECTED:
-          rejectFn(this.data)
-          break
-      }
-    })
-  }
-  catch(fn) {
-    return this.then(null, fn)
-  }
-  finally(callback) {
-    return this.then(
-      (res) => {
-        callback()
-        return res
-      },
-      (res) => {
-        callback()
-        throw res
-      }
-    )
-  }
-  resolve(value) {
-    return new Promise(resolve => resolve(value))
-  }
-  reject(value) {
-    return new Promise((reject, reject) => { this.reject(value) })
-  }
-}
+// class myPromise {
+//   constructor(fn) {
+//     this.status = 'pending'
+//     this.value = null
+//     this.resQueue = []
+//     this.rejQueue = []
+//     let resolve = (value) => {
+//       setTimeout(() => {
+//         if (this.status !== 'pending') return
+//         this.status = 'fulfilled'
+//         this.value = value
+//         for (let callback of this.resQueue) {
+//           callback(value)
+//         }
+//       })
+//     }
+//     let reject = (reason) => {
+//       setTimeout(() => {
+//         if (this.status !== PENDING) return
+//         this.status = REJECTED
+//         this.data = null
+//         for (let callback of this.rejQueue) {
+//           callback(reason)
+//         }
+//       })
+//     }
+//     fn(resolve, reject)
+//   }
+//   then(resFn, rejFn) {
+//     return new Promise((res, rej) => {
+//       let fulfilledFn = (v) => {
+//         try {
+//           res(resFn(v))
+//         } catch (error) {
+//           rej(error)
+//         }
+//       }
+//       let rejectedFn = (v) => {
+//         try {
+//           res(resFn(rejFn))
+//         } catch (error) {
+//           rej(error)
+//         }
+//       }
+//       switch (this.status) {
+//         case PENDING:
+//           this.resQueue.push(fulfilledFn);
+//           this.rejQueue.push(rejectedFn)
+//           break
+//         case FULFILLED:
+//           fulfilledfn(this.data)
+//           break
+//         case REJECTED:
+//           rejectFn(this.data)
+//           break
+//       }
+//     })
+//   }
+//   catch(fn) {
+//     return this.then(null, fn)
+//   }
+//   finally(callback) {
+//     return this.then(
+//       (res) => {
+//         callback()
+//         return res
+//       },
+//       (res) => {
+//         callback()
+//         throw res
+//       }
+//     )
+//   }
+//   resolve(value) {
+//     return new Promise(resolve => resolve(value))
+//   }
+//   reject(value) {
+//     return new Promise((reject, reject) => { this.reject(value) })
+//   }
+// }
 
 
 
@@ -103,3 +103,32 @@ new myPromise((resolve, reject) => {
 // result3 undefined
 
 
+
+
+
+class Promise {
+  constructor(fn) {
+    this.fn = fn
+    this.status = 'pending'
+    this.resQueue = []
+    this.rejQueue = []
+    let rej = (err) => {
+      for (let fn of this.rejQueue) {
+        fn(err)
+      }
+    }
+    let res = (v) => {
+      try {
+        for (let fn of this.resQueue) {
+          fn(v)
+        }
+      }
+      catch (e) {
+        rej(err)
+      }
+    }
+
+    fn(res, rej)
+    return this
+  }
+}
