4ae08c2933198d4c0e9d6d6ac083f8418c35f808
Bhwa233 <404174262@qq.com>
2023-11-16 14:42:45 +0800

    commit 2023年11月16日 14:42:45

--- 2-arrayArrangement.js
+++ 2-arrayArrangement.js
@@ -1,8 +1,6 @@
 // 给定任意二维数组，输出所有的排列组合项。
 // 比如 [['A','B'], ['a','b'], [1, 2]]，输出 ['Aa1','Aa2','Ab1','Ab2','Ba1','Ba2','Bb1','Bb2']
 
-
-
 const permutate = (arr) => {
   let res = arr[0]
   for (let i = 1; i < arr.length; i++) {
