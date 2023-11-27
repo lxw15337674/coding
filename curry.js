15de7ffb064e1965883c0698fbf3778891d1ffb4
bhwa233 <404174262@qq.com>
2023-11-27 12:15:41 +0800

    commit 2023年11月27日 12:15:41

--- curry.js
+++ curry.js
@@ -1,10 +1,10 @@
 // 判断当前传入函数的参数个数 (args.length) 是否大于等于原函数所需参数个数 (fn.length) ，如果是，则执行当前函数；如果是小于，则返回一个函数。例如：实现add(1)(2)(3)
 
 function curry(fn, ...currentArgs) {
-  const length = fn.length
+  const len = fn.length
   return (...args) => {
     const argsList = [...currentArgs, ...args]
-    if (argsList.length < length) {
+    if (argsList.length < len) {
       return curry(fn, ...argsList)
     } else {
       return fn(...argsList)
