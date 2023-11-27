3a294f4c69ac142c74f3bcfe90426c595a47f317
bhwa233 <404174262@qq.com>
2023-11-27 12:09:31 +0800

    commit 2023年11月27日 12:09:31

--- arraytoTree.js
+++ arraytoTree.js
@@ -1,34 +1,60 @@
 // 数组转树结构
 const transform = (arr) => {
-  const map = new Map();
-  const result = [];
+  // 方法一，先全部存到map里，然后再进行节点挂载，parent为0则为根节点。
+  // 时间复杂度O(2N)
+  // let tree = null;
+  // for (let node of arr) {
+  //   map.set(node.id, node);
+  // }
+  // for (let node of arr) {
+  //   if (node.parentId === 0) {
+  //     tree = [node];
+  //     continue;
+  //   }
+  //   const parentNode = map.get(node.parentId);
+  //   if (!parentNode.child) {
+  //     parentNode.child = [];
+  //   }
+  //   parentNode.child.push(node);
+  // }
+  // return tree;
 
-  for (let i = 0; i < arr.length; i++) {
-    const { id, parentId, name } = arr[i];
-
-    let current = map.get(id);
-    let parent = map.get(parentId);
-
-    if (!current) {
-      current = { id, name, children: [] };
-      map.set(id, current);
+  // 方法二，遍历时就进行挂载，如果父节点不存在，就先造一个只有id的父节点，当节点已存在，就只需要把节点的属性赋值到前面造的节点上
+  // 时间复杂度O（N）
+  const map = new Map()
+  let tree = null
+  for (let node of arr) {
+    // 当前节点处理，先判断map里是否已存在，如果已经存在就是之前已经造过的，进行属性合并，不存在就存到map里
+    let currentNode = map.get(node.id)
+    if (!currentNode) {
+      currentNode = node
+      map.set(currentNode.id, currentNode)
     } else {
-      current.name = name;
+      Object.assign(currentNode, node);
     }
 
-    if (!parent) {
-      parent = { id: parentId, name: null, children: [] };
-      map.set(parentId, parent);
+    // 父节点处理，如果不存在就造一个父节点，用来挂载。
+    let parentNode = map.get(currentNode.parentId)
+    // 如果不存在就造一个节点
+    if (!parentNode) {
+      parentNode = {
+        id: currentNode.parentId,
+      }
+      map.set(parentNode.id, parentNode)
     }
 
-    if (parentId === 0) {
-      result.push(current);
-    } else {
-      parent.children.push(current);
+    // 节点挂载，根节点就挂在tree上，否则就挂载在父节点上
+    // 根节点
+    if (currentNode.parentId === 0) {
+      tree = [currentNode]
+      continue
     }
+    if (!parentNode.child) {
+      parentNode.child = []
+    }
+    parentNode.child.push(currentNode)
   }
-
-  return result;
+  return tree
 }
 
 const arr = [
@@ -74,4 +100,6 @@ const arr = [
   },
 ]
 
-console.log(transform(arr))
+const tree = transform(arr)
+console.log(tree);
+
