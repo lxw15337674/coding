79cb6da0ae4eb524600af546c636ca1b0db022ed
Bhwa233 <404174262@qq.com>
2023-12-08 17:45:04 +0800

    commit 2023年12月 8日 17:45:04

--- deepClone.js
+++ deepClone.js
@@ -1,5 +1,7 @@
 // 如果是基本数据类型，则直接返回，如果是引用数据类型则循环每个item，递归复制
 // 处理循环引用，把每次创建的数据都存在map里，key为原对象，value为复制后的对象，每次复制前判断，如果存在则直接返回复制后的对象。
+// https://vue3js.cn/interview/JavaScript/copy.html#%E5%9B%9B%E3%80%81%E5%8C%BA%E5%88%AB
+
 function deepCopy(obj, visited = new WeakMap()) {
   if (obj === null || typeof obj !== 'object') {
     return obj;
@@ -8,23 +10,24 @@ function deepCopy(obj, visited = new WeakMap()) {
   if (visited.has(obj)) {
     return visited.get(obj);
   }
-  let clone;
+  let clone = null;
   if (Array.isArray(obj)) {
     clone = [];
-    visited.set(obj, clone); // 添加到 visited 中
-    for (let i = 0; i < obj.length; i++) {
-      clone[i] = deepCopy(obj[i], visited);
+    // 要提前存，在循环后存会无限递归。
+    visited.set(obj, clone);
+    for (let item of obj) {
+      clone.push(deepCopy(item, visited));
     }
   } else {
     clone = {};
-    visited.set(obj, clone); // 添加到 visited 中
+    // 要提前存，在循环后存会无限递归。
+    visited.set(obj, clone);
     for (let key in obj) {
       clone[key] = deepCopy(obj[key], visited);
     }
   }
   return clone;
 }
-
 
 
 // 测试用例
@@ -35,7 +38,10 @@ const originalObject = {
     city: 'New York',
     country: 'USA'
   },
-  hobbies: ['reading', 'traveling']
+  hobbies: ['reading', 'traveling'],
+  test: () => {
+    console.log('test');
+  }
 };
 
 // 创建循环引用
