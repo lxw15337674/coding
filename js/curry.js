ea32bb9ccbf4c29f31ba5f7c575271101d715ab5
Bhwa233 <404174262@qq.com>
2024-02-23 17:47:38 +0800

    commit 2024年02月23日 17:47:38

--- js/curry.js
+++ js/curry.js
@@ -1,23 +1,19 @@
 // 判断当前传入函数的参数个数 (args.length) 是否大于等于原函数所需参数个数 (fn.length) ，
 // 如果是，则执行当前函数；如果是小于，则返回一个函数。例如：实现add(1)(2)(3)
-
-const curry = (fn) => {
-  const curried = (...args) => {
-    if (args.length >= fn.length) {
-      return fn(...args);
-    } else {
-      return (...moreArgs) => {
-        return curried(...args, ...moreArgs);
-      };
+// 这个地方不能使用箭头函数，否则this的值可能会被绑定到错误的上下文。
+const currying = function (fn, ...args) {
+  if (args.length >= fn.length) {
+    return fn(...args);
+  } else {
+    return function (...moreArgs) {
+      return currying(fn, ...args, ...moreArgs);
     };
-  };
-  return curried;
+  }
 };
 
-const add = curry((a, b, c) => {
+const add = currying((a, b, c) => {
   console.log(a, b, c)
 })
 
 add(1)(2)(3)
 add(1, 2)(3)
-
